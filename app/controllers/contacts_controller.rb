9062b54e4cd520b845e0e9d55a65d5bd0184a1a9
kyleblee <hello.kylelee@gmail.com>
2017-11-16 14:43:06 -0500

    Build 'edit contact' feature

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -25,6 +25,7 @@ class ContactsController < ApplicationController
       redirect '/contacts/new'
     else
       if valid_email?(params) || params[:email] == ""
+        params.delete_if {|key, value| value == ""}
         @contact = Contact.create(name: params[:name], location: params[:location], email: params[:email], phone_number: params[:phone_number], category: params[:category], expertise: params[:expertise], preferences: params[:preferences])
         current_user.contacts << @contact
         redirect "/contacts/#{@contact.id}"
@@ -38,9 +39,29 @@ class ContactsController < ApplicationController
   get '/contacts/:id/edit' do
     @contact = Contact.find_by_id(params[:id])
     if logged_in? && @contact.user_id == current_user.id
-
+      erb :'contacts/edit'
     else
-      redirect '/users/signup'
+      redirect '/users/login'
+    end
+  end
+
+  patch '/contacts/:id' do
+    @contact = Contact.find_by_id(params[:id])
+    if params[:name] == ""
+      flash[:message] = "Please enter a name for the updated contact."
+      redirect "/contacts/#{@contact.id}/edit"
+    elsif Contact.find_by(name: params[:name]) && @contact.name != params[:name]
+      flash[:message] = "A contact already has that name. <br>Try including a last name so they don't get mixed up."
+      redirect "/contacts/#{@contact.id}/edit"
+    else
+      if valid_email?(params) || params[:email] == ""
+        params.delete_if {|key, value| value == ""}
+        @contact.update(name: params[:name], location: params[:location], email: params[:email], phone_number: params[:phone_number], category: params[:category], expertise: params[:expertise], preferences: params[:preferences])
+        redirect "/contacts/#{@contact.id}"
+      else
+        flash[:message] = "That email doesn't seem to be valid."
+        redirect "/contacts/#{@contact.id}/edit"
+      end
     end
   end
 
