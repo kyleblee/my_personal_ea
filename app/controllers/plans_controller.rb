3d65af428e4f125624963accc10ee047002fb0b9
kyleblee <hello.kylelee@gmail.com>
2017-11-19 20:16:56 -0500

    Add 'plans/:id/edit' view and logic

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -33,9 +33,58 @@ class PlansController < ApplicationController
         params[:contact][:existing_contacts].each {|name| @plan.contacts << Contact.find_by(name: name)}
       end
       unless params[:contact][:new_contact_name].empty?
-        @plan.contacts << Contact.create(name: params[:contact][:new_contact_name])
+        @new_contact = Contact.create(name: params[:contact][:new_contact_name])
+        @plan.contacts << @new_contact
+        current_user.contacts << @new_contact
       end
       current_user.plans << @plan
+      redirect "/plans/#{@plan.id}"
+    end
+  end
+
+  get '/plans/:id/edit' do
+    @plan = Plan.find_by_id(params[:id])
+    if logged_in? && @plan.user == current_user
+      erb :'plans/edit'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  patch '/plans/:id' do
+    @plan = Plan.find_by_id(params[:id])
+
+    if params[:name].empty?
+      flash[:message] = "Please enter a name for this plan."
+      redirect "/plans/#{@plan.id}/edit"
+    elsif Plan.find_by(name: params[:name]) && @plan.name != params[:name]
+      flash[:message] = "You already have a plan with that name.<br>Please provide a different name for this plan."
+      redirect "/plans/#{@plan.id}/edit"
+    elsif current_user.contacts.include?(Contact.find_by(name: params[:contact][:new_contact_name]))
+      flash[:message] = "You already have an existing contact with that name.<br>Please choose this contact from the contacts checklist."
+      redirect "/plans/#{@plan.id}/edit"
+    else
+      params.delete_if {|k, v| v == ""}
+      @plan.update(name: params[:name], date: params[:date], time: params[:time], location: params[:location], context: params[:context], pre_notes: params[:pre_notes], post_notes: params[:post_notes])
+
+      if params[:contact][:existing_contacts] == nil
+        @plan.contacts.clear
+      else
+        @plan.contacts.each do |contact|
+          @plan.contacts.delete(contact) if !params[:contact][:existing_contacts].include?(contact.name)
+        end
+        params[:contact][:existing_contacts].each do |name|
+          contact = Contact.find_by(name: name)
+          @plan.contacts << contact unless @plan.contacts.include?(contact)
+        end
+      end
+
+      unless params[:contact][:new_contact_name].empty?
+        @new_contact = Contact.create(name: params[:contact][:new_contact_name])
+        @plan.contacts << @new_contact
+        current_user.contacts << @new_contact
+      end
+
       redirect "/plans/#{@plan.id}"
     end
   end
@@ -47,7 +96,6 @@ class PlansController < ApplicationController
     else
       redirect '/users/login'
     end
-
   end
 
   helpers do
