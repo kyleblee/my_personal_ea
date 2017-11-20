49ae79589f3db7f343068c1b4012fd8a7720b1cb
kyleblee <hello.kylelee@gmail.com>
2017-11-20 10:33:46 -0500

    Fix another syntax error in environment file

--- config/environment.rb
+++ config/environment.rb
@@ -18,8 +18,8 @@ configure :production do
     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host => db.host,
     :username => db.user,
-    :password => db.password;
+    :password => db.password,
     :database => db.path[1..-1],
-    :encoding => utf8
+    :encoding => 'utf8'
   )
 end
